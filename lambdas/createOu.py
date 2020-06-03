4f15a08433b85f63814186ebb860087c544719db
kmorgan <kiki.morgan13@gmail.com>
2020-06-03 05:01:22 -0400

    refactor createOu function

--- lambdas/createOu.py
+++ lambdas/createOu.py
@@ -1,14 +1,10 @@
 import json
 import boto3
 from uuid import uuid4
-import os
 
-lambdaClient = boto3.client('lambda')
 ssm = boto3.client('ssm')
-sns = boto3.client('sns')
 org = boto3.client('organizations')
 
-#business logic
 
 def grabMasterId():
   
@@ -26,72 +22,38 @@ def createOrgUnit(rootId, ouName):
   
   return ouResponse['OrganizationalUnit']['Id']
 
-#communication logic
-
-def invoke(funct):
-  payload = {
-    "origin": "createOu"
-  }
-
-  response = lambdaClient.invoke(
-    FunctionName = funct,
-    InvocationType = 'Event',
-    LogType = 'None',
-    Payload = json.dumps(payload)
-  )
-
-
-
-def slackPublish(arn, status, function, text):
-  payload = {
-    "condition": status,
-    "function": function,
-    "text": text
-  }
-  response = sns.publish(
-    TopicArn = arn, 
-    Message=json.dumps({'default': json.dumps(payload)}),
-    MessageStructure='json'
-  ) 
-
+# pre-handler global
+masterId = grabMasterId()
 
 def lambda_handler(event, context):
-  lambdaName = os.environ['LambdaName']
-  topicArn = os.environ['SlackArn']
-  nextFunct = os.environ['InvokeLambda']
 
-
-  try:
-    
-    masterId = grabMasterId()
-    masterName = '/org-assemble/orgIds/master-'+ str(uuid4())
-    
-    putParameter = ssm.put_parameter(
-      Name = masterName,
-      Description = 'Master Org Id',
-      Value = masterId,
-      Type = 'String'
-    )
+  masterName = '/org-assemble/orgIds/master-'+ str(uuid4())
   
-    securityId = createOrgUnit(masterId, "Security")
-    workloadsId = createOrgUnit(masterId, "Workloads")
-    
-    securityName = '/org-assemble/orgIds/security-'+ str(uuid4())
-    putParameter = ssm.put_parameter(
-      Name = securityName,
-      Description = 'Security Ou Id',
-      Value = securityId,
-      Type = 'String'
-    )
-    
-    workloadsName = '/org-assemble/orgIds/workloads-'+ str(uuid4())
-    putParameter = ssm.put_parameter(
-      Name = workloadsName,
-      Description = 'Workloads Ou Id',
-      Value = workloadsId,
-      Type = 'String'
-    )
+  putParameter = ssm.put_parameter(
+    Name = masterName,
+    Description = 'Master Org Id',
+    Value = masterId,
+    Type = 'String'
+  )
 
-    invoke(nextFunct)
-  except:
-    slackPublish(topicArn, "failed", lambdaName, None)
+  workloadsId = createOrgUnit(masterId, "Workloads")
+  
+  workloadsName = '/org-assemble/orgIds/workloads-'+ str(uuid4())
+  
+  putParameter = ssm.put_parameter(
+    Name = workloadsName,
+    Description = 'Workloads Ou Id',
+    Value = workloadsId,
+    Type = 'String'
+  )
+
+  securityId = createOrgUnit(masterId, "Security")
+
+  securityName = '/org-assemble/orgIds/security-'+ str(uuid4())
+  
+  putParameter = ssm.put_parameter(
+    Name = securityName,
+    Description = 'Security Ou Id',
+    Value = securityId,
+    Type = 'String'
+  )
